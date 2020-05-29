d5489302e5e2a81153dc9c9eecdd6ebada53cb91
airnix09 <airnix09@users.noreply.github.com>
2020-05-29 22:29:59 +0200

    update start script

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./assets/data.json";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const result = [];
+
+  data.map((elem) => {
+    const title = elem.title;
+    const symbol = elem.symbol;
+
+    if (title.includes(search)) {
+      result.push(
+        <div className="line">
+          <div>
+            <span>{symbol}</span>
+            <span>{title}</span>
+          </div>
+          <span
+            onClick={() => {
+              navigator.clipboard.writeText(symbol);
+            }}
+          >
+            copy to clipboard
+          </span>
+        </div>
+      );
+    }
+
+    return result;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <span>&#128526; EmojiSearch &#128526;</span>
+        <input
+          placeholder="What emoji are you looking for ?"
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="lines">{result}</div>
+      <div className="footer">
+        <span>Training project Made with React by H. RAJAONARISON</span>
+      </div>
     </div>
   );
 }
